85c122961d6f813ad229b3c7f0b2d5de89a5751a
Jordano <jgtomazoni@ucs.br>
2023-08-20 11:43:41 -0300

    WIP - Lógica de paginação implementada para todos os controllers;

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -14,9 +14,11 @@ class CommentController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $comments = Comment::query()->get();
+
+        $pageSize = $request->page_size ?? 20;
+        $comments = Comment::query()->pagination($pageSize);
 
         return CommentResource::collection($comments);
     }
